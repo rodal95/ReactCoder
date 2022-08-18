0a282d164b9ed68bab8b80b373614e8e87562e5c
rodal95 <103323215+rodal95@users.noreply.github.com>
2022-08-18 16:23:34 -0300

    desafio contador

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
-
+let stock = 10
+let cantidadElegida = 0
 const Counter = () =>{
     const [count, setcount]= useState(0);
     function Restar(){
@@ -7,11 +8,26 @@ const Counter = () =>{
             setcount(count-1)
         }
     }
+    function sumar(){
+      if(count < stock){
+        setcount(count+1)
+      }
+    }
+    function agregar(){
+      if(count != 0){
+        cantidadElegida=count
+        stock = stock - cantidadElegida
+        return {cantidadElegida, stock}
+      }
+    }
   return (<div>
     <span>Numero de clicks <b>{count}</b></span><br></br>
+    <span>Stock disponible{stock}</span><br />
+    <span id="Stockelegido">La cantidad elegida es {cantidadElegida}</span><br />
     <div  style={{display:'flex', flexDirection:"row", justifyContent:"center", justifyContent:"space-evenly"}}>
-    <button onClick={()=>setcount(count+1)}>+</button>
+    <button onClick={sumar}>+</button>
     <button onClick={()=>setcount(0)}>Limpiar</button>
+    <button onClick={agregar}>Agregar</button>
     <button onClick={Restar}>-</button>
     </div>
     </div>
