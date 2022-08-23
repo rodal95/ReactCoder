0713174fc010295a2b7d08ca5abe50c1ee6102d5
rodal95 <103323215+rodal95@users.noreply.github.com>
2022-08-23 18:02:44 -0300

    arreglando desafio

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 export default function Item({id,title, price}) {
   return (
     <div>
-        
-        <h3>{title}</h3><br />
-       <b><span>US${price}</span></b><br />
+        <Card style={{width: '18rem'}} >
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>
+          <span>{price}</span>
+          <span>{}</span>
+        </Card.Text>
+        <Button variant="primary">Comprar</Button>
+      </Card.Body>
+    </Card>
+       
     </div>
   )
 }
